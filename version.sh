a0c6b76fec9ef3f03b45e59a4e7b8288fca6950f
Rob Kooper <kooper@illinois.edu>
2024-05-01 10:31:09 -0500

    fix version script

--- version.sh
+++ version.sh
@@ -2,7 +2,7 @@
 
 VERSION=0.3.0
 
-sed -i~ "s#SYSTEM_VERSION=.*#SYSTEM_VERSION=\"${VERSION}\" \\\\#" server/Dockerfile
+sed -i~ "s#SYSTEM_VERSION=.*#SYSTEM_VERSION=\"${VERSION}\" \\\\#" cdrhook/Dockerfile
 
 sed -i~ "s#SYSTEM_VERSION=.*#SYSTEM_VERSION=\"${VERSION}\"#" .env
 sed -i~ "s#SYSTEM_VERSION=.*#SYSTEM_VERSION=\"${VERSION}\"#" env.example
