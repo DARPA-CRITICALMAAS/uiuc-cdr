f2ae63e21f0075c8127fb7810a14b466237ab0d1
Albert Bode <abode@illinois.edu>
2024-07-09 12:13:17 -0500

    formatted tests and adding missing __init__.py

--- tests/test_cdrhook/test_connector.py
+++ tests/test_cdrhook/test_connector.py
@@ -6,29 +6,39 @@ from cdrhook.connector import CdrConnector
 
 load_dotenv()
 
+
 class TestCDRConnector:
-    system_name = 'ncsa_test'
-    system_version = '0.1'
-    token = os.getenv('CDR_TOKEN')
-    callback_url = 'https://criticalmaas.ncsa.illinois.edu/'
+    system_name = "ncsa_test"
+    system_version = "0.1"
+    token = os.getenv("CDR_TOKEN")
+    callback_url = "https://criticalmaas.ncsa.illinois.edu/"
 
     def test_constructor(self):
-        log = init_test_log('TestCDRConnector/test_constructor')
-        con = CdrConnector(system_name=self.system_name, system_version=self.system_version, token=self.token, callback_url=self.callback_url)
-        
-        log.info(f'Connector : {con.__dict__}')
+        log = init_test_log("TestCDRConnector/test_constructor")
+        con = CdrConnector(
+            system_name=self.system_name,
+            system_version=self.system_version,
+            token=self.token,
+            callback_url=self.callback_url,
+        )
+
+        log.info(f"Connector : {con.__dict__}")
         assert con.system_name == self.system_name
         assert con.system_version == self.system_version
         assert con.token == self.token
         assert str(con.callback_url) == self.callback_url
-        log.info('Test passed successfully')
+        log.info("Test passed successfully")
 
     def test_registration(self):
-        log = init_test_log('TestCDRConnector/test_registration')
-        con = CdrConnector(system_name=self.system_name, system_version=self.system_version, token=self.token, callback_url=self.callback_url)
+        log = init_test_log("TestCDRConnector/test_registration")
+        con = CdrConnector(
+            system_name=self.system_name,
+            system_version=self.system_version,
+            token=self.token,
+            callback_url=self.callback_url,
+        )
         con.register()
         assert con.registration is not None
         con.unregister()
         assert con.registration is None
-        log.info('Test passed successfully')
-        
+        log.info("Test passed successfully")
--- tests/test_cdrhook/test_convert.py
+++ tests/test_cdrhook/test_convert.py
@@ -1,9 +1,14 @@
 import json
+
 import cdrhook.convert as convert
-from cdr_schemas.map_results import MapResults
-from cdrhook.cdr_endpoint_schemas import AreaExtractionsEndpoint, LegendItemsEndpoint, MetadataEndpoint
-from cdrhook.retrieve import validate_endpoint
 from tests.utilities import init_test_log
+from cdrhook.retrieve import validate_endpoint
+# from cdr_schemas.map_results import MapResults
+from cdrhook.cdr_endpoint_schemas import (
+    AreaExtractionsEndpoint,
+    LegendItemsEndpoint,
+    MetadataEndpoint,
+)
 
 class TestConvert:
     # def test_convert_cdr_schema_map_to_cmass_map(self):
@@ -18,29 +23,29 @@ class TestConvert:
     #     log.info('Test passed successfully')
 
     def test_convert_cdr_schema_metadata_to_cmass_map_metadata(self):
-        log = init_test_log('TestConvert/test_convert_cdr_schema_metadata_to_cmass_map_metadata')
-        with open('tests/data/sample_cog_metadata.json', 'r') as fh:
-            cdr_metadata = (validate_endpoint(json.load(fh), MetadataEndpoint))
+        log = init_test_log("TestConvert/test_convert_cdr_schema_metadata_to_cmass_map_metadata")
+        with open("tests/data/sample_cog_metadata.json", "r") as fh:
+            cdr_metadata = validate_endpoint(json.load(fh), MetadataEndpoint)
         cmass_map_metadata = convert.convert_cdr_schema_metadata_to_cmass_map_metadata(cdr_metadata)
         assert cmass_map_metadata
-        log.info('Test passed successfully')
+        log.info("Test passed successfully")
 
     def test_convert_cdr_schema_legend_items_to_cmass_legend(self):
-        log = init_test_log('TestConvert/test_convert_cdr_schema_legend_items_to_cmass_legend')
-        with open('tests/data/sample_cog_legend.json', 'r') as fh:
+        log = init_test_log("TestConvert/test_convert_cdr_schema_legend_items_to_cmass_legend")
+        with open("tests/data/sample_cog_legend.json", "r") as fh:
             cdr_legend = []
             for map_unit in json.load(fh):
                 cdr_legend.append(validate_endpoint(map_unit, LegendItemsEndpoint))
         cmass_legend = convert.convert_cdr_schema_legend_items_to_cmass_legend(cdr_legend)
         assert cmass_legend
-        log.info('Test passed successfully')
+        log.info("Test passed successfully")
 
     def test_convert_cdr_schema_area_extraction_to_layout(self):
-        log = init_test_log('TestConvert/test_convert_cdr_schema_area_extraction_to_layout')
-        with open('tests/data/sample_cog_area_extraction.json', 'r') as fh:
+        log = init_test_log("TestConvert/test_convert_cdr_schema_area_extraction_to_layout")
+        with open("tests/data/sample_cog_area_extraction.json", "r") as fh:
             cdr_area_extraction = []
             for area in json.load(fh):
                 cdr_area_extraction.append(validate_endpoint(area, AreaExtractionsEndpoint))
         layout = convert.convert_cdr_schema_area_extraction_to_layout(cdr_area_extraction)
         assert layout
-        log.info('Test passed successfully')
+        log.info("Test passed successfully")
