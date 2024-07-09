a2f663fa57a96630d976d113447e257945ee4d1a
Albert Bode <abode@illinois.edu>
2024-07-09 11:50:18 -0500

    Trimed sample data to resonable size

--- tests/data/mock_data.py
+++ tests/data/mock_data.py
@@ -1,15 +1,18 @@
 from cdrhook.connector import CdrConnector
 
+
 class MockConnector(CdrConnector):
-    # override 
+    # override
     def register(self):
-        return '12345'
+        return "12345"
+
     # override
     def unregister(self):
-        return 
-    
+        return
+
     def __eq__(self, other):
         return self.__dict__ == other.__dict__
 
+
 def get_mock_connector():
-    return MockConnector('mock_connector', '0.1', 'mock_token')
+    return MockConnector("mock_connector", "0.1", "mock_token")
